e48abb7097a4546b41826502216b43be14f2939f
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-09-25 20:51:42 +0000

    Compile kyoto cabinet and add sample code to server.c.

--- sessions/hack-kyoto-mochi/server.c
+++ sessions/hack-kyoto-mochi/server.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 #include <margo.h>
 #include "types.h"
+#include "kclangc.h"
+
+//// kyoto cabinet
+/* call back function for an existing record */
+const char* visitfull(const char* kbuf, size_t ksiz,
+                      const char* vbuf, size_t vsiz, size_t *sp, void* opq) {
+  fwrite(kbuf, 1, ksiz, stdout);
+  printf(":");
+  fwrite(vbuf, 1, vsiz, stdout);
+  printf("\n");
+  return KCVISNOP;
+}
+
+/* call back function for an empty record space */
+const char* visitempty(const char* kbuf, size_t ksiz, size_t *sp, void* opq) {
+  fwrite(kbuf, 1, ksiz, stdout);
+  printf(" is missing\n");
+  return KCVISNOP;
+}
+
+//// kyoto cabinet
 
 typedef struct {
     int max_rpcs;
@@ -13,6 +34,69 @@ DECLARE_MARGO_RPC_HANDLER(sum)
 
 int main(int argc, char** argv)
 {
+
+  //// kyoto cabinet
+
+  KCDB* db;
+  KCCUR* cur;
+  char *kbuf, *vbuf;
+  size_t ksiz, vsiz;
+  const char *cvbuf;
+
+  /* create the database object */
+  db = kcdbnew();
+
+  /* open the database */
+  if (!kcdbopen(db, "casket.kch", KCOWRITER | KCOCREATE)) {
+    fprintf(stderr, "open error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* store records */
+  if (!kcdbset(db, "foo", 3, "hop", 3) ||
+      !kcdbset(db, "bar", 3, "step", 4) ||
+      !kcdbset(db, "baz", 3, "jump", 4)) {
+    fprintf(stderr, "set error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* retrieve a record */
+  vbuf = kcdbget(db, "foo", 3, &vsiz);
+  if (vbuf) {
+    printf("%s\n", vbuf);
+    kcfree(vbuf);
+  } else {
+    fprintf(stderr, "get error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* traverse records */
+  cur = kcdbcursor(db);
+  kccurjump(cur);
+  while ((kbuf = kccurget(cur, &ksiz, &cvbuf, &vsiz, 1)) != NULL) {
+    printf("%s:%s\n", kbuf, cvbuf);
+    kcfree(kbuf);
+  }
+  kccurdel(cur);
+
+  /* retrieve a record with visitor */
+  if (!kcdbaccept(db, "foo", 3, visitfull, visitempty, NULL, 0) ||
+      !kcdbaccept(db, "dummy", 5, visitfull, visitempty, NULL, 0)) {
+    fprintf(stderr, "accept error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* traverse records with visitor */
+  if (!kcdbiterate(db, visitfull, NULL, 0)) {
+    fprintf(stderr, "iterate error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* close the database */
+  if (!kcdbclose(db)) {
+    fprintf(stderr, "close error: %s\n", kcecodename(kcdbecode(db)));
+  }
+
+  /* delete the database object */
+  kcdbdel(db);
+
+  //// kyoto cabinet
+
     margo_instance_id mid = margo_init("na+sm", MARGO_SERVER_MODE, 0, 0);
     assert(mid);
 
