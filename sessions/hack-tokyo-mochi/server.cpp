8fa104273703e84392c84b8d9bc9aabd7dea73a7
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-09-26 21:10:52 +0000

    Create base files of tokyo-mochi.

---
+++ sessions/hack-tokyo-mochi/server.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+void sum(const tl::request& req, int x, int y) {
+  std::cout << "Computing " << x << "+" << y << std::endl;
+  req.respond(x+y);
+}
+
+int main(int argc, char** argv) {
+
+  tl::engine myEngine("na+sm", THALLIUM_SERVER_MODE);
+  std::cout << "Server running at address " << myEngine.self() << std::endl;
+  myEngine.define("sum", sum);
+
+  return 0;
+}
